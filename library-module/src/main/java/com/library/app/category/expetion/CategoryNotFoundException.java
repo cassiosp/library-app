d8278af619e0c648472d1f44520aa0e1ccb82e57
Cassio Pereira <cassio@shockmonkeystudios.com>
2016-03-15 17:17:59 -0300

    CategoryServices implementation finishing with new tests, finder methods and code improvement.

---
+++ library-module/src/main/java/com/library/app/category/expetion/CategoryNotFoundException.java
@@ -0,0 +1,6 @@
+package com.library.app.category.expetion;
+
+public class CategoryNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = -6583863597490595623L;
+
+}
--- library-module/src/test/java/com/library/app/category/services/impl/CategoryServicesUTest.java
+++ library-module/src/test/java/com/library/app/category/services/impl/CategoryServicesUTest.java
@@ -5,6 +5,10 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.validation.Validation;
 import javax.validation.Validator;
 
@@ -12,6 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.library.app.category.expetion.CategoryExistentException;
+import com.library.app.category.expetion.CategoryNotFoundException;
 import com.library.app.category.model.Category;
 import com.library.app.category.repository.CategoryRepository;
 import com.library.app.category.services.CategoryServices;
@@ -65,9 +70,94 @@ public class CategoryServicesUTest {
         assertThat(category.getId(), is(equalTo(1L)));
     }
 
+    @Test
+    public void updateCategoryWithNullName() {
+        updateCategoryWithInvalidName(null);
+    }
+
+    @Test
+    public void updateCategoryWithShortName() {
+        updateCategoryWithInvalidName("A");
+    }
+
+    @Test
+    public void updateCategoryWithLongName() {
+        updateCategoryWithInvalidName("This is a long name that will cause an exception to be thrown");
+    }
+
+    @Test(expected = CategoryExistentException.class)
+    public void updateCategoryWithExistingName() {
+        when(categoryRepository.alreadyExists(categoryWithId(java(), 1L))).thenReturn(true);
+
+        categoryServices.update(categoryWithId(java(), 1L));
+    }
+
+    @Test(expected = CategoryNotFoundException.class)
+    public void updateCategoryNotFound() {
+        when(categoryRepository.alreadyExists(categoryWithId(java(), 1L))).thenReturn(false);
+        when(categoryRepository.existsById(1L)).thenReturn(false);
+
+        categoryServices.update(categoryWithId(java(), 1L));
+    }
+
+    @Test
+    public void updateValidCategory() {
+        when(categoryRepository.alreadyExists(categoryWithId(java(), 1L))).thenReturn(false);
+        when(categoryRepository.existsById(1L)).thenReturn(true);
+
+        categoryServices.update(categoryWithId(java(), 1L));
+
+        verify(categoryRepository).update(categoryWithId(java(), 1L));
+    }
+
+    @Test
+    public void findCategoryById() {
+        when(categoryRepository.findById(1L)).thenReturn(categoryWithId(java(), 1L));
+
+        final Category category = categoryServices.findById(1L);
+        assertThat(category, is(notNullValue()));
+        assertThat(category.getId(), is(equalTo(1L)));
+        assertThat(category.getName(), is(equalTo(java().getName())));
+    }
+
+    @Test(expected = CategoryNotFoundException.class)
+    public void findCategoryByIdNotFound() {
+        when(categoryRepository.findById(1L)).thenReturn(null);
+
+        categoryServices.findById(1L);
+    }
+
+    @Test
+    public void findAllNoCategories() {
+        when(categoryRepository.findAll("name")).thenReturn(new ArrayList<Category>());
+
+        final List<Category> categories = categoryServices.findAll();
+        assertThat(categories.isEmpty(), is(equalTo(true)));
+    }
+
+    @Test
+    public void findAllCategories() {
+        when(categoryRepository.findAll("name")).thenReturn(
+                Arrays.asList(categoryWithId(java(), 1L), categoryWithId(networks(), 2L)));
+
+        final List<Category> categories = categoryServices.findAll();
+        assertThat(categories.size(), is(equalTo(2)));
+        assertThat(categories.get(0).getName(), is(equalTo(java().getName())));
+        assertThat(categories.get(1).getName(), is(equalTo(networks().getName())));
+    }
+
     private void addCategoryWithInvalidName(String name) {
         try {
             categoryServices.add(new Category(name));
+            fail("An error should have been thrown");
+        } catch (final FieldNotValidException e) {
+            assertThat(e.getFieldName(), is(equalTo("name")));
+        }
+    }
+
+    private void updateCategoryWithInvalidName(String name) {
+        try {
+            categoryServices.update(new Category(name));
             fail("An error should have been thrown");
         } catch (final FieldNotValidException e) {
             assertThat(e.getFieldName(), is(equalTo("name")));
